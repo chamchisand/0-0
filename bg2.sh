703cb5a7588c503bca9771e7c54a796799870abc
Andrew Kang <andrew716@gmail.com>
2018-01-26 22:55:18 -0800

    bg2

--- bg2.sh
+++ bg2.sh
@@ -1,90 +1,81 @@
 #!/bin/bash
 
+print() {
+  echo $1
+}
+
 error() {
-  echo $1 1>&2;
+  echo $1 1>&2
+  exit 1
 }
 
-usage() {
-  error "Usage: $0 [-b background] [-r resize] file"
-}
-
-while getopts ":b:m:r:" o; do
-  case "$o" in
-    m) mode="$OPTARG" ;;
-    b) bg="$OPTARG" ;;
-    r) resize="$OPTARG" ;;
-    *) usage; exit 1 ;;
-  esac
-done
-
-shift $((OPTIND-1))
-mode=${mode:=max}
-bg=${bg:=#000000}
-
-if [ -z "$1" ]; then
-  usage; exit 1;
-fi
-
-if [ -d "$1" ]; then
-  echo "Randomly pick background from $1"
-  feh --no-fehbg --bg-$mode --randomize $1
-  exit
-fi
-
-if [ ! -f "$1" ]; then
-  error "ERROR: invalid file path"
-fi
-
-img=$(readlink -f "$1")
-
-echo "img=$img"
-echo "bg=$bg"
-echo "mode=$mode"
-echo "resize=$resize"
-
-line=$(file $img | grep -Po '\d+\s*x\s*\d+' | tail -1)
-IFS=x read -ra size <<< $line
-w=${size[0]}
-h=${size[1]}
-echo img: $w x $h
-
-screen=$(xrandr | grep -P '\*' | grep -Po '\d+\s*x\s*\d+')
-IFS=x read -ra screenSize <<< $screen
-screenWidth=${screenSize[0]}
-screenHeight=${screenSize[1]}
-echo "screen: $screenWidth x $screenHeight"
-
-if [ -z "$mode" ]; then
-  if [ $w -lt $screenWidth ] && [ $h -lt $screenHeight ]; then
-    mode=center
-  else
-    mode=max
-  fi
+if [ ! "$(which feh 2> /dev/null)" ]; then
+  error ERROR: feh not installed
 fi
 
 if [ ! "$(which convert 2> /dev/null)" ]; then
-  echo ERROR: ImageMagick not installed
-  exit 1
+  error ERROR: ImageMagick not installed
 fi
+
+if [ -z "$1" ]; then
+  error "ERROR: path to image or directory is missing"
+fi
+
+if [ -d "$1" ]; then
+  files=($1/*)
+  img=${files[RANDOM % ${#files[@]}]}
+elif [ -f "$1" ]; then
+  img=$(readlink -f "$1")
+else
+  error "ERROR: invalid file path"
+fi
+
+shift
+
+bg="#000000"
+resize="100%"
+verbose=0
+
+while getopts ":b:r:v" o; do
+  case "$o" in
+    v) verbose=1 ;;
+    b) bg="$OPTARG" ;;
+    r) resize="$OPTARG" ;;
+    *) echo "Usage: $0 [-b background] [-r resize] file"; exit 1 ;;
+  esac
+done
+
+screen=$(xrandr | grep -P '\*' | grep -Po '\d+\s*x\s*\d+')
+IFS=x read -ra screenSize <<< $screen
+width=${screenSize[0]}
+height=${screenSize[1]}
 
 filename=$(basename "$img")
 dir=$(dirname "$img")
 ext="${filename##*.}"
 filename="${filename%.*}"
-output="$dir/background-$filename-$RANDOM.$ext"
-echo "output=$output"
+output="$dir/background.$ext"
 
+if [ "$verbose" = "1" ]; then
+  line=$(file $img | grep -Po '\d+\s*x\s*\d+' | tail -1)
+  IFS=x read -ra size <<< $line
+  w=${size[0]}
+  h=${size[1]}
 
-if [ -z "$resize" ]; then
-  resize="${screenWidth}x${screenHeight}"
+  echo "image path: $img"
+  echo "image size: $w x $h"
+  echo "screen size: $width x $height"
+  echo "background color: $bg"
+  echo "resize: $resize"
 fi
 
 convert $img \
   -quality 100 \
+  -geometry ${width}x${height} \
   -gravity center \
   -resize $resize \
   -background "$bg" \
-  -extent ${screenWidth}x${screenHeight} \
+  -extent ${width}x${height} \
   $output
 
 feh --no-fehbg --bg-center $output
