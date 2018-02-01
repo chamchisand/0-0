41cad6db871407d341ba3bb427cd380a1e6b2e46
Andrew Kang <andrew716@gmail.com>
2018-01-31 23:46:55 -0800

    wallpaper

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,46 +1,51 @@
 #!/bin/bash
 
-print() {
-  echo $1
-}
-
 error() {
   echo $1 1>&2
   exit 1
 }
 
+usage() {
+  echo "Usage: $0 <path> [-b background color] [-r resize]
+Options:
+  -b background color [default: background color from Xresources]
+  -r image size [default: 100%]"
+  exit 1
+}
+
 if [ ! "$(which feh 2> /dev/null)" ]; then
-  error ERROR: feh not installed
+  error ERROR: feh is not installed
 fi
 
 if [ ! "$(which convert 2> /dev/null)" ]; then
-  error ERROR: ImageMagick not installed
+  error ERROR: ImageMagick is not installed
 fi
 
 if [ -z "$1" ]; then
-  error "ERROR: path to image or directory is missing"
+  usage
 fi
-
-if [ -d "$1" ]; then
-  files=($1/*)
-  img=${files[RANDOM % ${#files[@]}]}
-elif [ -f "$1" ]; then
-  img=$(readlink -f "$1")
-else
-  error "ERROR: invalid file path"
-fi
-
-shift
 
 resize="100%"
 verbose=0
+path=$1
+shift
+
+if [ -d "$path" ]; then
+  path=$(find $path -type f -regex '.*\.\(jpg\|jpeg\|png\)$' | shuf | head -n 1)
+fi
+
+if [ -f "$path" ]; then
+  path=$(readlink -f "$path")
+else
+  error "ERROR: $path is not valid file path"
+fi
 
 while getopts ":b:r:v" o; do
   case "$o" in
     v) verbose=1 ;;
     b) bg="$OPTARG" ;;
     r) resize="$OPTARG" ;;
-    *) echo "Usage: $0 [-b background] [-r resize] file"; exit 1 ;;
+    *) usage; exit 1 ;;
   esac
 done
 
@@ -53,32 +58,33 @@ IFS=x read -ra screenSize <<< $screen
 width=${screenSize[0]}
 height=${screenSize[1]}
 
-filename=$(basename "$img")
-dir=$(dirname "$img")
+filename=$(basename "$path")
+dir=$(dirname "$path")
 ext="${filename##*.}"
 filename="${filename%.*}"
-output="$dir/background.$ext"
+output="/tmp/wallpaper.$ext"
 
 if [ "$verbose" = "1" ]; then
-  line=$(file $img | grep -Po '\d+\s*x\s*\d+' | tail -1)
+  line=$(file $path | grep -Po '\d+\s*x\s*\d+' | tail -1)
   IFS=x read -ra size <<< $line
   w=${size[0]}
   h=${size[1]}
 
-  echo "image path: $img"
+  echo "image path: $path"
   echo "image size: $w x $h"
   echo "screen size: $width x $height"
   echo "background color: $bg"
   echo "resize: $resize"
 fi
 
-convert $img \
+convert $path \
   -quality 100 \
   -geometry ${width}x${height} \
   -gravity center \
   -resize $resize \
-  -background "$bg" \
+  -background "${bg:-#000000}" \
   -extent ${width}x${height} \
   $output
 
 feh --no-fehbg --bg-center $output
+rm $output
